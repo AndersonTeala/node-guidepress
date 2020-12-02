a5759e83d428c101b3232e0ebaa4166ca6dc641a
AndersonTeala <andersonteala@gmail.com>
2020-12-02 14:21:57 -0300

    Create connection database

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
+
+/* View engine */
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+/* Body parser */
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+/* Database */
+
+connection
+    .authenticate()
+    .then(() => {
+      console.log("✅ Connected to the database!");
+    }).catch((error) => {
+      console.log(error);
+    })
 
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
 
 app.get("/", (req,res) => {
-  res.send("🚀 Hello World!");
+  res.render("index");
 })
 
 
